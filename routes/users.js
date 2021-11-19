e366c33c53e77603412186c95d861b7ad96d0335
Alessandro Magionami <alessandro.magionami@gmail.com>
2021-11-19 18:37:48 +0100

    users list and detail

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,16 @@ import SQL from '@nearform/sql'
 
 export default async function (fastify, opts) {
   fastify.get('/users', async function (request, reply) {
-    const query = SQL`` // INSERIRE QUI LA QUERY
+    const query = SQL`SELECT id, lastName, firstName, username FROM users` // INSERIRE QUI LA QUERY
 
     const [rows] = await fastify.pool.query(query)
     return rows
+  })
+
+  fastify.get('/users/:id', async function (request, reply) {
+    const query = SQL`SELECT id, lastName, firstName, username FROM users WHERE id = ${request.params.id}` // INSERIRE QUI LA QUERY
+
+    const [rows] = await fastify.pool.query(query)
+    return rows[0]
   })
 }
